7cb9774d87c39edb04255620832f71221911c67e
chhitizzz <chhitizvaidya10@gmail.com>
2024-09-20 11:37:39 +0545

    automating for post request

--- GOREST API/RequestAPI.py
+++ GOREST API/RequestAPI.py
@@ -12,6 +12,7 @@ auth_token = "Bearer dcbf5d16c6a97aaf3eeaa873c411c8edf18d6673edba15ee65ed2c03ba6
 # GET Request
 def get_request():
     url = base_url + "/public/v2/users/"
+    print(f"GET Request URL: {url}")
     headers = {"Authorization": auth_token}
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
@@ -20,6 +21,21 @@ def get_request():
     print(f"json response body: {json_str}")
 
 # POST Request 
+def post_requests():
+    url = base_url + "/public/v2/users/"
+    print(f"POST Request URL: {url}")
+    headers = {"Authorization": auth_token}
+    data = {
+        "name": "API Automation",
+        "email": "testautomation@test.com",
+        "gender": "male",
+        "status": "active"
+    }
+    response = requests.post(url, json=data, headers=headers)
+    assert response.status_code == 201
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print(f"json response body: {json_str}")
 
 # PUT Request 
 
